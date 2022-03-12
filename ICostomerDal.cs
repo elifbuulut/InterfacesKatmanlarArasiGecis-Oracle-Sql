648c45ce6c2df111a8d73d075ec3b61654c2db7e
elifb <elifb@DESKTOP-R9ITCEQ>
2022-03-12 19:51:03 +0300

    Add project files.

---
+++ ICostomerDal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesKatmanlarArasiGecis_Oracle_Sql
+{
+    public interface ICostomerDal
+    
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+    class SqlServerCustomerDal : ICostomerDal
+
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+
+        }
+    }
+
+    class OracleCustomerDal : ICostomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated");
+        }
+    }
+
+
+    class MySqlCustomerDal : ICostomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql Updated");
+        }
+    }
+    public class CustomerManager
+    {
+
+        public void Add(ICostomerDal costomer)
+        {
+            costomer.Add();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesKatmanlarArasiGecis_Oracle_Sql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.Add(new SqlServerCustomerDal());
+            //customerManager.Add(new OracleCustomerDal());
+
+            // YUKARDA Kİ KULLANIM YERİNE INTERFACEDEN BİR DİZİ OLUŞTURUP TEK TEK EKLEME İŞLEMLERİNİ YAPTIRIYORUZ 
+
+            //new'lenen şey bir ICustomerDal nesnesi değil, içinde ICustomerDal implemente eden classları tutan bir Array nesnesi.
+            //Yani en başta Array'i new'ledik, sonra ise
+            //class'ları new'ledik, ICustomerDal'ı new'lemedik. Zaten ICustomerDal'ı implemente eden somut class'lar new'lenebilir.
+            //Bu şekilde düşünebiliriz
+            // SqlServerCustomerDal sqlServerCustomerDal=new SqlServerCustomerDal();
+            //OracleCustomerDal oracleCustomerDal = new OracleCustomerDal();   şeklinde de yapabilirdik
+            ICostomerDal[] costomerDals = new ICostomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+              
+
+            };
+
+            foreach (var customerDaal in costomerDals)
+            {
+                customerDaal.Add();
+            };
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
