93f3df7ef01f6679f9a7e922808c9ec8b54914e5
Elif Bulut <96698984+elifbuulut@users.noreply.github.com>
2022-03-23 04:04:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,12 +17,6 @@ namespace InterfacesKatmanlarArasiGecis_Oracle_Sql
 
             // YUKARDA Kİ KULLANIM YERİNE INTERFACEDEN BİR DİZİ OLUŞTURUP TEK TEK EKLEME İŞLEMLERİNİ YAPTIRIYORUZ 
 
-            //new'lenen şey bir ICustomerDal nesnesi değil, içinde ICustomerDal implemente eden classları tutan bir Array nesnesi.
-            //Yani en başta Array'i new'ledik, sonra ise
-            //class'ları new'ledik, ICustomerDal'ı new'lemedik. Zaten ICustomerDal'ı implemente eden somut class'lar new'lenebilir.
-            //Bu şekilde düşünebiliriz
-            // SqlServerCustomerDal sqlServerCustomerDal=new SqlServerCustomerDal();
-            //OracleCustomerDal oracleCustomerDal = new OracleCustomerDal();   şeklinde de yapabilirdik
             ICostomerDal[] costomerDals = new ICostomerDal[3]
             {
                 new SqlServerCustomerDal(),
